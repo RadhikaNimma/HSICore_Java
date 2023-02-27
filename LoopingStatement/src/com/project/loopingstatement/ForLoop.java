c766f194fd89d074b86af0f2a3f3cc9eb088bcd2
PARVEEN <PARVEEN@DESKTOP-LTP9U3N>
2023-02-27 11:32:09 +0530

    SecondProject

---
+++ LoopingStatement/src/com/project/loopingstatement/ForLoop.java
@@ -0,0 +1,13 @@
+package com.project.loopingstatement;
+
+public class ForLoop {
+	public static void main(String[] args) {
+		for (int i=1;i<=10;i++) {
+			if ( i % 2 == 0) {
+			System.out.println(i);		
+		
+	}
+
+}
+}
+}
---
+++ LoopingStatement/src/com/project/loopingstatement/ForLoop1.java
@@ -0,0 +1,18 @@
+package com.project.loopingstatement;
+
+public class ForLoop1 {
+	public static void main(String[] args) {
+     for (int i = 1; i <= 4; i++) {
+    	 System.out.print(i + "");
+    	 
+    	 
+}
+   System.out.println();
+   for (
+		   int i =5; i<=8; i++) {
+	   System.out.print(i +" ") ;
+	   
+   }
+
+	}
+}
